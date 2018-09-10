c1c01c3faefd08c2eed804c39d046c30b322a1ed
André Carlos <andre-carlos@live.com>
2018-09-10 19:47:38 -0300

    update things

--- website/urls.py
+++ website/urls.py
@@ -5,8 +5,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', home, name='Home'),
-    path('login/', auth_views.LoginView.as_view(template_name="login.html"), name='Login'),
-    path('cadastro/', signup, name='Cadastro'),
+    path('login/', auth_views.LoginView.as_view(template_name="login.html"), name='login'),
+    path('cadastro/', signup, name='cadastro'),
     path('logout/', auth_views.LogoutView.as_view(next_page= '/'), name='logout'),
     path('perfil/<int:id>/', perfil, name='perfil')
 ]
