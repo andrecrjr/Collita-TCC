4e098410665efaa58fa85e293b5350cf9f70d39b
André Carlos <andre-carlos@live.com>
2018-09-16 19:53:40 -0300

    One more field to json

--- apisite/serializers.py
+++ apisite/serializers.py
@@ -6,7 +6,8 @@ class ProfileSerializer(serializers.ModelSerializer):
     profile_id = serializers.CharField(source='user.id')
     username = serializers.CharField(source='user.username')
     first_name = serializers.CharField(source='user.first_name')
+    last_name = serializers.CharField(source='user.last_name')
     moeda = serializers.CharField(source='inventario_usuario.moeda')
     class Meta:
         model = Profile
-        fields = ('profile_id','username', 'first_name', 'moeda')
+        fields = ('profile_id','username', 'first_name','last_name', 'moeda', )
