cda97d96e1b7a9739a00a25d4ed4f1060ac09d79
André Carlos <andre-carlos@live.com>
2018-09-16 20:07:22 -0300

    Changing url for perfil

--- apisite/urls.py
+++ apisite/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', ProfileList.as_view(), name='Profile api'),
-    path('<int:pk>/', ProfileDetails.as_view()),
+    path('perfil/<int:pk>/', ProfileDetails.as_view()),
     path('item/', ItemList.as_view(), name='Items api'),
     path('item/<int:pk>/', ItemDetails.as_view(), name='Items api')
 ]
