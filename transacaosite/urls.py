6e23353737aa1a35b1b23c5021adfb644ab78539
André Carlos <andre-carlos@live.com>
2019-04-08 20:28:27 -0300

    correcting front end

--- transacaosite/urls.py
+++ transacaosite/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('add_item/', add_item_cart, name='add_item'),
     path('list_item/', list_items, name='list_items'),
     path('delete/', delete_cart, name='delete'),
-    path('finalizar/', generate_boleto, name='finalizar'),
+    path('generate_boleto/', generate_boleto, name='generate_boleto'),
     path('create_boleto/', request_boleto, name='request_boleto'),
     path('boleto_pay/', paid_boleto, name='paid_boleto')
 ]
