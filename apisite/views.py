46a4d200dae90f4c80b6d38774c4ac077560850d
André Carlos <andre-carlos@live.com>
2019-04-08 21:26:39 -0300

    greater than 0 in itens serializer list

--- apisite/views.py
+++ apisite/views.py
@@ -11,7 +11,7 @@ class InventarioItemList(generics.ListAPIView):
     authentication_classes = (TokenAuthentication,)
     def get_queryset(self):
         user = self.kwargs['pk']
-        return ItemCompra.objects.filter(id_usuario=user)
+        return ItemCompra.objects.filter(id_usuario=user, quantidade__gt=0)
 
 
 class InventarioDetails(generics.RetrieveAPIView):
