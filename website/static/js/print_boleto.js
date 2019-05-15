db70940217b79df3fd8c753effbe1b2c23760934
André Carlos <andre-carlos@live.com>
2019-05-15 12:13:17 -0300

    one more alteration to boleto transaction

--- website/static/js/print_boleto.js
+++ website/static/js/print_boleto.js
@@ -2,36 +2,37 @@ const boletoPrint = document.querySelector(".print-boleto")
 
 boletoPrint.addEventListener('click', function print(){
     let div = document.querySelector('.cada-boleto')
-    return printElement(div)
+    return printElement(this,div)
 })
 
-const printElement = (div) =>{
+const printElement = (elem, div) =>{
+    elem.disabled = true
     let printPage = window.open('', 'PRINT', 'height=400, width=600')
-    printPage.document.write(div.children[1].innerHTML);
+    //printPage.document.write(div.children[1].children[1].textContent);
+    //printPage.document.write(div.children[1].children[2].textContent);
     
-/*
-    printPage.document.write(`<tbody>
-    <tr>
-            <td>Código:</td>
-            <td> ${div.children}</td>
-    </tr>
-    <tr>
-            <td>Status do boleto:</td>
-            <td>{{ boleto.status_boleto }}</td>
-    </tr>
-    <tr>
-        <td>Data de expedição:</td>
-        <td>{{ boleto.data_boleto_criado }}</td>
-    </tr>
-    <tr>
-    </tr>
-            <td>Data de vencimento:</td>
-            <td>{{ boleto.expiration_boleto_date }}</td>
-    </tr>
-</tbody>`)
-*/
+    printPage.document.write(`
+    <table style="border:1px black solid">
+        <tbody>
+            <tr>
+                    <td>${div.children[1].children[0].textContent}</td>
+            </tr>
+            <tr>
+                   <td>${div.children[1].children[1].textContent}</td>
+            </tr>
+            <tr>
+                <td>${div.children[1].children[2].textContent}</td>
+            </tr>
+            <tr>
+            </tr>
+                    <td>${div.children[1].children[3].textContent}</td>
+            </tr>
+        </tbody>
+    </table>
+`)
     printPage.print()
     printPage.close()
+    
     return true
 }
 
